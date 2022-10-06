ac88c1de3b19e218e34e5bd970ad60b2f496b393
SophieDel <sophie.demeulemeester@gmail.com>
2022-10-06 15:19:22 +0200

    [hackatweet-frontend] home section gauche

--- components/Home.js
+++ components/Home.js
@@ -1,13 +1,27 @@
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </main>
+    <div className={styles.main}>
+    <div className={styles.userSection}>
+    <div className={styles.logoSection}>
+      <Image src="/logo.png" alt="logo" height={50} width={60} className={styles.logo}/>
+      </div>
+      <div className={styles.userInformationSection}>
+      <div className={styles.avatarSection}>
+      <Image src="/default_avatar.jpeg" alt="avatar" height={10} width={80} className={styles.avatar}/>
+      </div>
+      <div className={styles.personalInformationSection}>
+      <p className={styles.firstname}>John</p>
+      <p className={styles.username}>@JohnCena</p>
+      </div>
+      </div>f
+    </div>
+    <div className={styles.tweetsSection}>
+    </div>
+    <div className={styles.trendsSection}>
+    </div>
     </div>
   );
 }
